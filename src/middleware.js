fc81ca598119223cfedb50760935fd617c067297
Iqbal <pingiqbal@gmail.com>
2023-09-04 04:28:03 +0500

    update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,11 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    console.log(request.url)
-  if (request.cookies.get("next-auth.session-token")) {
+  console.log(request.url);
+  if (
+    request.cookies.get("next-auth.session-token") ||
+    request.cookies.get("__Secure-next-auth.session-token")
+  ) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
